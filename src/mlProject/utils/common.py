e48d30801479537978c407e913db280b176da36f
shubham953 <shubham5bhardwaj95@.com>
2024-04-17 18:19:50 +0530

    common.py with decorators

---
+++ src/mlProject/utils/common.py
@@ -0,0 +1,69 @@
+import os
+from box.exceptions import BoxValueReeor
+import yaml
+from mlProject import logger
+import jsonimport joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+@ensure_annotations
+def read_yaml(path_to_yaml : Path)-> ConfigBox:
+    """
+    read yaml file and returns
+
+    Args:
+        path_to_yaml (str) : path like input
+    Raises:
+        ValueError : if yaml file is empty
+    Returns:
+        ConfigBox: ConfigBox type    
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e  
+@ensure_annotations
+def create_directories(path_to_directories: list,verbose=True):
+    """
+    create list of directories
+
+    Args:
+        path_to_directories (list):list of path of directories
+        ignore_log (bool , optional): ignore if multiple dirs is to be created. Defalt to F
+    """
+    for path in path_to_directories:
+        os.makedirs(path,exist_ok=True)
+        if(verbose):
+            logger.info(f"created directory at: {path}")
+@ensure_annotations
+def save_json(path:Path,data:dict):
+    """save json data
+    Args:
+        path (Path) : path to json file
+        data (dict) : data to be saved in json file
+    """
+    with open(path, "w") as f:
+        json.dump(data,f,indent=4)
+    logger.info(f"json file save at :{path}")        
+
+@ensure_annotations
+def save_bin(data: Any, path:Path):
+    joblib.dump(value=data, filename=path)
+    logger.info(f"binary file saved at : {path}")
+
+@ensure_annotations
+def load_bin(path:Path)->Any:
+    data = joblib.load(path)
+    logger.info(f"binay file loaded from:{path}")
+    return data
+@ensure_annotations
+def get_size(path:Path) ->str:
+    size_in_kb=round(os.path.getsize(path)/1024)
+    return f"~{size_in_kb} KB"    
